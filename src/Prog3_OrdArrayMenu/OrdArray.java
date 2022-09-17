c488794b2d4e8b01bc410c6f550e348a8a411cd4
deybyr647 <deybyr647@gmail.com>
2022-09-17 18:13:16 -0400

    Finished adding methods for program 3

--- src/Prog3_OrdArrayMenu/OrdArray.java
+++ src/Prog3_OrdArrayMenu/OrdArray.java
@@ -11,7 +11,8 @@ public class OrdArray {
         elementCount = 0;
     }
 
-    public void fillArray(int amount) throws IllegalArgumentException {
+    // 1. Fill Array
+    public void fill(int amount) throws IllegalArgumentException {
         if(amount < 1 || amount > 10) {
             throw new IllegalArgumentException("amount must be between 1 & 10");
         }
@@ -22,17 +23,93 @@ public class OrdArray {
             int randomNumber = getRandomNumber(1, 99);
             collection[i] = randomNumber;
         }
+
+        if(elementCount > 1) {
+            ascendingSort();
+        }
+    }
+
+    // 2. Add Number
+    public int insert() {
+        if (elementCount == collection.length) {
+            System.out.println("Couldn't add a new number because array is full!");
+            return -1;
+        }
+
+        int newNumber = getRandomNumber(1, 99);
+        collection[elementCount] = newNumber;
+        elementCount++;
+
+        return newNumber;
+    }
+
+    // 3. Find the index of a number
+    public int find(int num) {
+        int index = -1;
+
+        for(int i = 0; i < elementCount; i++){
+            if(collection[i] == num){
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    // 4. Remove number at index
+    public int removeNumberAtIndex(int index) {
+        if(index < 0 || index >= elementCount){
+            return -1;
+        }
+
+        int itemAtIndex = collection[index];
+        System.out.println("Index: " + index);
+        System.out.println("Item at index: " + itemAtIndex);
+
+        for(int i = index; i < elementCount - 1; i++) {
+            collection[i] = collection[i + 1];
+        }
+
+        elementCount--;
+
+        return itemAtIndex;
+    }
+
+    // 5. Remove number
+    public int removeNumber(int num) {
+        int indexOfNum = find(num);
+
+        if(indexOfNum == -1){
+            return -1;
+        }
+
+        return removeNumberAtIndex(indexOfNum);
     }
 
     private int getRandomNumber(int min, int max) {
         return (int) (Math.floor(Math.random() * (max - min + 1)) + min);
     }
 
-    private void sort() {
+    private void ascendingSort() {
+        int temp;
 
+        for(int i = 0; i < elementCount; i++){
+            for(int j = i + 1; j < elementCount; j++){
+                if(collection[i] > collection[j]){
+                    temp = collection[i];
+                    collection[i] = collection[j];
+                    collection[j] = temp;
+                }
+            }
+        }
     }
 
     public int[] toArray() {
         return Arrays.copyOf(collection, elementCount);
+    }
+
+    public int getElementCount(){
+        return elementCount;
     }
 }
--- src/Prog3_OrdArrayMenu/OrdArrayApp.java
+++ src/Prog3_OrdArrayMenu/OrdArrayApp.java
@@ -5,9 +5,13 @@ import java.util.Arrays;
 public class OrdArrayApp {
     public static void main(String[] args) {
         OrdArray test = new OrdArray();
-        test.fillArray(1);
+        test.fill(10);
 
         String arrString = Arrays.toString(test.toArray());
+        System.out.println(arrString);
+
+        test.removeNumberAtIndex(9);
+        arrString = Arrays.toString(test.toArray());
         System.out.println(arrString);
     }
 }
